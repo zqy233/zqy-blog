d899e459df585a194344b7a76d3eca2f71507d54
zqylzcwcxy <347891134@qq.com>
2022-03-02 22:55:54 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_basic_mockjs.md.c4476b1b.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.f2aaa6dd.js";
+const __pageData = '{"title":"mockjs","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4E0B\u8F7D\u5B89\u88C5","slug":"\u4E0B\u8F7D\u5B89\u88C5"},{"level":2,"title":"\u4F7F\u7528","slug":"\u4F7F\u7528"}],"relativePath":"notes/basic/mockjs.md","lastUpdated":1646232842034}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 8);
+const _hoisted_9 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_9);
+}
+var mockjs = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, mockjs as default };
