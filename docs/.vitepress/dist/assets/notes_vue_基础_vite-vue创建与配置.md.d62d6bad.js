fd1a21de2d98e8e2ab534b05f8bb6876f0e8712d
zqylzcwcxy <347891134@qq.com>
2022-03-05 14:40:02 +0800

    ':memo:撰写文档'

--- docs/.vitepress/dist/assets/notes_vue_基础_vite-vue创建与配置.md.d62d6bad.js
+++
@@ -1,8 +0,0 @@
-import { _ as _export_sfc, c as createElementBlock, o as openBlock } from "./app.88d7bab7.js";
-const __pageData = '{"title":"","description":"","frontmatter":{},"relativePath":"notes/vue/\u57FA\u7840/vite-vue\u521B\u5EFA\u4E0E\u914D\u7F6E.md","lastUpdated":1646447683835}';
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return openBlock(), createElementBlock("div");
-}
-var viteVue_____ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
-export { __pageData, viteVue_____ as default };
