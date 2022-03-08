387c71ec7eb7cd6ac59e7fd8c9309a276cace7f5
zqylzcwcxy <347891134@qq.com>
2022-03-08 15:23:25 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_5.other_1.vscode_4.vscode插件api.md.6bb56fd6.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.93a48cac.js";
+const __pageData = '{"title":"vscode\u63D2\u4EF6api","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5B98\u65B9\u82F1\u6587\u6587\u6863","slug":"\u5B98\u65B9\u82F1\u6587\u6587\u6863"},{"level":2,"title":"languages","slug":"languages"},{"level":2,"title":"registerDocumentFormattingEditProvider","slug":"registerdocumentformattingeditprovider"},{"level":2,"title":"registerDocumentHighlightProvider","slug":"registerdocumenthighlightprovider"},{"level":2,"title":"window","slug":"window"},{"level":3,"title":"window.showOpenDialog","slug":"window-showopendialog"}],"relativePath":"notes/5.other/1.vscode/4.vscode\u63D2\u4EF6api.md","lastUpdated":1646575178208}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 28);
+const _hoisted_29 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_29);
+}
+var _4_vscode__api = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _4_vscode__api as default };
---
+++ docs/.vitepress/dist/assets/notes_5.other_3.solve-problems_ps.md.8828e3ec.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.93a48cac.js";
+const __pageData = '{"title":"ps","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u6682\u5B58\u76D8\u5DF2\u6EE1","slug":"\u6682\u5B58\u76D8\u5DF2\u6EE1"},{"level":2,"title":"\u53F3\u4FA7\u9762\u677F\u6298\u53E0","slug":"\u53F3\u4FA7\u9762\u677F\u6298\u53E0"},{"level":2,"title":"\u4E0D\u80FD\u79FB\u52A8\u9009\u533A\uFF0C\u56E0\u4E3A\u5DF2\u9009\u5B9A\u753B\u677F","slug":"\u4E0D\u80FD\u79FB\u52A8\u9009\u533A\uFF0C\u56E0\u4E3A\u5DF2\u9009\u5B9A\u753B\u677F"},{"level":2,"title":"\u8BBE\u7F6E\u5F62\u72B6\u989C\u8272","slug":"\u8BBE\u7F6E\u5F62\u72B6\u989C\u8272"}],"relativePath":"notes/5.other/3.solve-problems/ps.md","lastUpdated":1646397526743}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 17);
+const _hoisted_18 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_18);
+}
+var ps = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, ps as default };
