86c3336310e34996064ddd34bf5f25c16e009cef
zqylzcwcxy <347891134@qq.com>
2022-03-10 21:23:08 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_6.other_3.solve-problems_vue.md.00603636.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.b64a0083.js";
+const __pageData = '{"title":"vue","description":"","frontmatter":{},"headers":[{"level":2,"title":"Critical dependency: the request of a dependency is an expression","slug":"critical-dependency-the-request-of-a-dependency-is-an-expression"},{"level":2,"title":"for division is deprecated and will be removed in Dart Sass 2.0.0","slug":"for-division-is-deprecated-and-will-be-removed-in-dart-sass-2-0-0"}],"relativePath":"notes/6.other/3.solve-problems/vue.md","lastUpdated":1646397535233}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 10);
+const _hoisted_11 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_11);
+}
+var vue = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, vue as default };
