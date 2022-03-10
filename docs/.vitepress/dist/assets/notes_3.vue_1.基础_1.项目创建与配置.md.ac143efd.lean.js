d3e9104158a23ded418354411911169612b87369
zqylzcwcxy <347891134@qq.com>
2022-03-10 21:03:49 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_3.vue_1.基础_1.项目创建与配置.md.ac143efd.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.5284f575.js";
+const __pageData = '{"title":"\u9879\u76EE\u521B\u5EFA\u4E0E\u914D\u7F6E","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4F7F\u7528@vue/cli \u521B\u5EFA\u9879\u76EE","slug":"\u4F7F\u7528-vue-cli-\u521B\u5EFA\u9879\u76EE"},{"level":2,"title":"\u4F7F\u7528 create-vite \u521B\u5EFA\u9879\u76EE","slug":"\u4F7F\u7528-create-vite-\u521B\u5EFA\u9879\u76EE"},{"level":2,"title":"\u5B89\u88C5 vue devtools \u8C37\u6B4C\u63D2\u4EF6","slug":"\u5B89\u88C5-vue-devtools-\u8C37\u6B4C\u63D2\u4EF6"},{"level":2,"title":"vue-router","slug":"vue-router"},{"level":3,"title":"\u4E0B\u8F7D\u4F7F\u7528","slug":"\u4E0B\u8F7D\u4F7F\u7528"},{"level":3,"title":"\u4F20\u53C2","slug":"\u4F20\u53C2"},{"level":3,"title":"\u8DEF\u7531\u5360\u4F4D\u7B26","slug":"\u8DEF\u7531\u5360\u4F4D\u7B26"},{"level":3,"title":"\u8DEF\u7531\u4F20\u9012 props \u7684\u4E09\u79CD\u5199\u6CD5","slug":"\u8DEF\u7531\u4F20\u9012-props-\u7684\u4E09\u79CD\u5199\u6CD5"},{"level":3,"title":"router-view","slug":"router-view"},{"level":2,"title":"vite \u4E2D\u56FE\u7247\u8DEF\u5F84","slug":"vite-\u4E2D\u56FE\u7247\u8DEF\u5F84"},{"level":2,"title":"unplugin-auto-import","slug":"unplugin-auto-import"},{"level":2,"title":"unplugin-vue-components","slug":"unplugin-vue-components"},{"level":2,"title":"vite-plugin-pages","slug":"vite-plugin-pages"},{"level":2,"title":"vite-plugin-md","slug":"vite-plugin-md"},{"level":2,"title":"prism-theme-vars","slug":"prism-theme-vars"},{"level":2,"title":"vite.config.ts","slug":"vite-config-ts"}],"relativePath":"notes/3.vue/1.\u57FA\u7840/1.\u9879\u76EE\u521B\u5EFA\u4E0E\u914D\u7F6E.md","lastUpdated":1646895161145}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 99);
+const _hoisted_100 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_100);
+}
+var _1________ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _1________ as default };
