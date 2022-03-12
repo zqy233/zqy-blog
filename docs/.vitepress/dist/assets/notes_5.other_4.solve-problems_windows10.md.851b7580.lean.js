5568c2126a3b2fa3e1c7fc7fd746ecec1e8e6933
zqylzcwcxy <347891134@qq.com>
2022-03-12 23:30:51 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_5.other_4.solve-problems_windows10.md.851b7580.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.2f2a8deb.js";
+const __pageData = '{"title":"windows10","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u89C6\u56FE\u64CD\u4F5C","slug":"\u89C6\u56FE\u64CD\u4F5C"},{"level":2,"title":"\u5FEB\u6377\u6253\u5F00\u8F6F\u4EF6","slug":"\u5FEB\u6377\u6253\u5F00\u8F6F\u4EF6"},{"level":2,"title":"cmd\u547D\u4EE4","slug":"cmd\u547D\u4EE4"},{"level":2,"title":"\u5FEB\u6377\u5173\u95EDcmd","slug":"\u5FEB\u6377\u5173\u95EDcmd"},{"level":2,"title":"\u6279\u91CF\u4FEE\u6539\u6587\u4EF6\u540D\u540E\u7F00","slug":"\u6279\u91CF\u4FEE\u6539\u6587\u4EF6\u540D\u540E\u7F00"},{"level":2,"title":"\u5173\u95ED\u201D\u4F60\u8981\u5141\u8BB8\u6765\u81EA\u672A\u77E5\u53D1\u5E03\u8005\u7684\u6B64\u5E94\u7528\u5BF9\u4F60\u7684\u8BBE\u5907\u8FDB\u884C\u66F4\u6539\u5417\uFF1F\u201C","slug":"\u5173\u95ED\u201D\u4F60\u8981\u5141\u8BB8\u6765\u81EA\u672A\u77E5\u53D1\u5E03\u8005\u7684\u6B64\u5E94\u7528\u5BF9\u4F60\u7684\u8BBE\u5907\u8FDB\u884C\u66F4\u6539\u5417\uFF1F\u201C"},{"level":2,"title":"yarn\u547D\u4EE4\u51FA\u73B0\u5728\u6B64\u7CFB\u7EDF\u4E0A\u7981\u6B62\u8FD0\u884C\u811A\u672C","slug":"yarn\u547D\u4EE4\u51FA\u73B0\u5728\u6B64\u7CFB\u7EDF\u4E0A\u7981\u6B62\u8FD0\u884C\u811A\u672C"},{"level":2,"title":"windows10\u53F3\u51FB\u6CA1\u6709\u7BA1\u7406\u5458\u8FD0\u884C","slug":"windows10\u53F3\u51FB\u6CA1\u6709\u7BA1\u7406\u5458\u8FD0\u884C"}],"relativePath":"notes/5.other/4.solve-problems/windows10.md","lastUpdated":1646966725470}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 24);
+const _hoisted_25 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_25);
+}
+var windows10 = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, windows10 as default };
