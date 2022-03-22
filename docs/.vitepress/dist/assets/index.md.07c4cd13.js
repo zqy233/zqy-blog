41ccd22fa42ad89a384ce6401f9e5b6e05e2524d
zqylzcwcxy <347891134@qq.com>
2022-03-22 22:06:12 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/index.md.07c4cd13.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.0e26bc01.js";
+var index_md_vue_type_style_index_0_scoped_true_lang = "";
+const __pageData = '{"title":"","description":"","frontmatter":{"page":true,"footer":false},"relativePath":"index.md","lastUpdated":1646791452898}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<section id="hero" data-v-738781ff><div class="tagline" data-v-738781ff><span class="accent" data-v-738781ff>\u4E2A\u4EBA\u7B14\u8BB0</span><br data-v-738781ff><p data-v-738781ff>fork from @vue/theme</p></div><p class="description" data-v-738781ff></p><p class="actions" data-v-738781ff><a class="get-started" href="/vite-vue-blog/notes/1.basic/1.html/1.\u57FA\u7840\u77E5\u8BC6" data-v-738781ff>\u5FEB\u901F\u5F00\u59CB <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 24 24" data-v-738781ff><path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" data-v-738781ff></path></svg></a></p></section><section id="highlights" class="vt-box-container" data-v-738781ff><div class="vt-box" data-v-738781ff><h2 data-v-738781ff>\u6613\u5B66\u6613\u7528</h2><p data-v-738781ff>\u57FA\u4E8Evue,\u4F7F\u7528vitepress\u4E0E@vue/theme</p></div><div class="vt-box" data-v-738781ff><h2 data-v-738781ff>\u6027\u80FD\u51FA\u8272</h2><p data-v-738781ff>\u57FA\u4E8Evite\uFF0C\u6309\u9700\u52A0\u8F7D\uFF0C\u6027\u80FD\u51FA\u8272\uFF0C\u6781\u901F\u66F4\u65B0\u6587\u6863</p></div><div class="vt-box" data-v-738781ff><h2 data-v-738781ff>\u4E3B\u9898\u6E32\u67D3</h2><p data-v-738781ff>\u6839\u636E\u914D\u7F6E\u6E32\u67D3\u9875\u9762\uFF0C\u4EA6\u53EF\u9009\u62E9\u4E3B\u9898\u4E0E\u5F00\u53D1\u81EA\u5B9A\u4E49\u4E3B\u9898</p></div></section>', 2);
+const _hoisted_3 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+var index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-738781ff"]]);
+export { __pageData, index as default };
--- push.bat
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-time=$(date "+%Y-%m-%d %H:%M:%S")
-git add .
-git commit -m ":memo:撰写文档 $time"
-git push
