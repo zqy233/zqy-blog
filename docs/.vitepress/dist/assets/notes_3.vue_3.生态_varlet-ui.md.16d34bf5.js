95c6124cb3b662e79fba5494576b1807bc7302a9
zqylzcwcxy <347891134@qq.com>
2022-03-27 14:42:31 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_3.vue_3.生态_varlet-ui.md.16d34bf5.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.0dcddf27.js";
+const __pageData = '{"title":"varlet-ui","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5982\u4F55\u8FD0\u884C\u9879\u76EE","slug":"\u5982\u4F55\u8FD0\u884C\u9879\u76EE"}],"relativePath":"notes/3.vue/3.\u751F\u6001/varlet-ui.md","lastUpdated":1646810529965}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="varlet-ui" tabindex="-1">varlet-ui <a class="header-anchor" href="#varlet-ui" aria-hidden="true">#</a></h1><p>\u5B66\u4E60varlet-ui\u7EC4\u4EF6\u5E93</p><h2 id="\u5982\u4F55\u8FD0\u884C\u9879\u76EE" tabindex="-1">\u5982\u4F55\u8FD0\u884C\u9879\u76EE <a class="header-anchor" href="#\u5982\u4F55\u8FD0\u884C\u9879\u76EE" aria-hidden="true">#</a></h2><p>1.git clone <a href="https://gitee.com/zqylzcwcxy/varlet.git" target="_blank" rel="noopener noreferrer">https://gitee.com/zqylzcwcxy/varlet.git</a></p><p><a href="http://2.cd" target="_blank" rel="noopener noreferrer">2.cd</a> packages</p><p>3.packages\u6587\u4EF6\u76EE\u5F55\u8BF4\u660E</p><table><thead><tr><th>\u6587\u4EF6\u5939\u540D</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td>varlet-cli</td><td>\u521B\u5EFA\u7EC4\u4EF6\u5E93\u6587\u6863\u7684\u811A\u624B\u67B6</td></tr><tr><td>varlet-icons</td><td>\u7EC4\u4EF6\u5E93\u56FE\u6807\u5217\u8868</td></tr><tr><td>varlet-ui</td><td>\u7EC4\u4EF6\u5E93</td></tr></tbody></table><p>4.pnpm dev</p>', 8);
+const _hoisted_9 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_9);
+}
+var varletUi = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, varletUi as default };
