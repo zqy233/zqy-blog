1c880c17973187929b0fa46b53601e14a984bd21
zqylzcwcxy <347891134@qq.com>
2022-03-28 21:27:59 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_3.vue_2.进阶_源码阅读.md.77e3f07d.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.b2510b57.js";
+const __pageData = '{"title":"VNode","description":"","frontmatter":{},"headers":[{"level":2,"title":"VNode","slug":"vnode"},{"level":3,"title":"\u662F\u4EC0\u4E48","slug":"\u662F\u4EC0\u4E48"},{"level":3,"title":"\u6709\u5565\u4F5C\u7528","slug":"\u6709\u5565\u4F5C\u7528"},{"level":3,"title":"\u7C7B\u578B","slug":"\u7C7B\u578B"}],"relativePath":"notes/3.vue/2.\u8FDB\u9636/\u6E90\u7801\u9605\u8BFB.md","lastUpdated":1645064430627}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 12);
+const _hoisted_13 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_13);
+}
+var ____ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, ____ as default };
