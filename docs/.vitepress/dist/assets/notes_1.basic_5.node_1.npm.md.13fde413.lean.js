c1c0a2cc36f37edd35399ae515d77fad58575423
zqylzcwcxy <347891134@qq.com>
2022-04-03 19:33:45 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_1.basic_5.node_1.npm.md.13fde413.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.d9e2e64d.js";
+const __pageData = '{"title":"npm","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u57FA\u7840\u7528\u6CD5","slug":"\u57FA\u7840\u7528\u6CD5"},{"level":2,"title":"\u4F7F\u7528yarn","slug":"\u4F7F\u7528yarn"},{"level":2,"title":"\u4F7F\u7528cnpm","slug":"\u4F7F\u7528cnpm"},{"level":2,"title":"\u4F7F\u7528pnpm","slug":"\u4F7F\u7528pnpm"},{"level":2,"title":"package.json","slug":"package-json"},{"level":3,"title":"\u521B\u5EFApackage.json","slug":"\u521B\u5EFApackage-json"},{"level":2,"title":"\u53D1\u5E03\u4E0E\u4E0B\u67B6 npm \u5305","slug":"\u53D1\u5E03\u4E0E\u4E0B\u67B6-npm-\u5305"},{"level":2,"title":"\u672C\u5730\u8C03\u8BD5\u7EC4\u4EF6\u5E93","slug":"\u672C\u5730\u8C03\u8BD5\u7EC4\u4EF6\u5E93"},{"level":2,"title":"\u4F9D\u8D56\u5305\u7248\u672C\u53F7~\u548C^\u7684\u533A\u522B","slug":"\u4F9D\u8D56\u5305\u7248\u672C\u53F7-\u548C-\u7684\u533A\u522B"},{"level":2,"title":"vue add \u548C npm i \u533A\u522B","slug":"vue-add-\u548C-npm-i-\u533A\u522B"},{"level":2,"title":"\u672C\u5730\u8FD0\u884C\u6253\u5305\u540E\u524D\u7AEF\u9879\u76EE","slug":"\u672C\u5730\u8FD0\u884C\u6253\u5305\u540E\u524D\u7AEF\u9879\u76EE"},{"level":2,"title":"nvm","slug":"nvm"}],"relativePath":"notes/1.basic/5.node/1.npm.md","lastUpdated":1647497400802}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 52);
+const _hoisted_53 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_53);
+}
+var _1_npm = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _1_npm as default };
