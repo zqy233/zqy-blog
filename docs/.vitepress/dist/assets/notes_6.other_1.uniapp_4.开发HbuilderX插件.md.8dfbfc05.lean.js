1a910f3f5d7c64009c7e7f87c036195594c42106
zqylzcwcxy <347891134@qq.com>
2022-04-15 17:13:55 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_6.other_1.uniapp_4.开发HbuilderX插件.md.8dfbfc05.lean.js
@@ -0,0 +1,13 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.cb380004.js";
+var _imports_0 = "/vite-vue-blog/assets/uniapp-3.e6a504d2.png";
+const __pageData = '{"title":"\u5F00\u53D1HbuilderX\u63D2\u4EF6","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u521B\u5EFA\u63D2\u4EF6\u9879\u76EE","slug":"\u521B\u5EFA\u63D2\u4EF6\u9879\u76EE"},{"level":2,"title":"package.json","slug":"package-json"},{"level":2,"title":"\u63D2\u4EF6\u4E3B\u51FD\u6570","slug":"\u63D2\u4EF6\u4E3B\u51FD\u6570"},{"level":2,"title":"\u63D2\u4EF6\u53D1\u5E03","slug":"\u63D2\u4EF6\u53D1\u5E03"},{"level":2,"title":"\u672C\u5730\u63D2\u4EF6","slug":"\u672C\u5730\u63D2\u4EF6"}],"relativePath":"notes/6.other/1.uniapp/4.\u5F00\u53D1HbuilderX\u63D2\u4EF6.md","lastUpdated":1649208679369}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 15);
+const _hoisted_16 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_16);
+}
+var _4___HbuilderX__ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _4___HbuilderX__ as default };
