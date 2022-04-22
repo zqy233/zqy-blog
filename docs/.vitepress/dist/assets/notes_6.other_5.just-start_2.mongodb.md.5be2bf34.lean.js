452e98c827b6d4ac302a22257e3bb80a6179c8e4
zqylzcwcxy <347891134@qq.com>
2022-04-22 15:19:53 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_6.other_5.just-start_2.mongodb.md.5be2bf34.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.c02d6b30.js";
+const __pageData = '{"title":"mongodb","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7B2C\u4E00\u6B65\uFF0C\u4E0B\u8F7D","slug":"\u7B2C\u4E00\u6B65\uFF0C\u4E0B\u8F7D"},{"level":2,"title":"\u7B2C\u4E8C\u6B65\uFF0C\u8BBE\u7F6E\u73AF\u5883\u53D8\u91CF\u4E3Abin\u76EE\u5F55","slug":"\u7B2C\u4E8C\u6B65\uFF0C\u8BBE\u7F6E\u73AF\u5883\u53D8\u91CF\u4E3Abin\u76EE\u5F55"},{"level":2,"title":"\u7B2C\u4E09\u6B65\uFF0Cmongodb\u5B89\u88C5\u76EE\u5F55\u4E0B\u65B0\u5EFA\u4E00\u4E2Amongo.config\u914D\u7F6E\u6587\u4EF6","slug":"\u7B2C\u4E09\u6B65\uFF0Cmongodb\u5B89\u88C5\u76EE\u5F55\u4E0B\u65B0\u5EFA\u4E00\u4E2Amongo-config\u914D\u7F6E\u6587\u4EF6"},{"level":2,"title":"\u7B2C\u56DB\u6B65\uFF0C\u521B\u5EFA\u51E0\u4E2A\u6587\u4EF6\u5939","slug":"\u7B2C\u56DB\u6B65\uFF0C\u521B\u5EFA\u51E0\u4E2A\u6587\u4EF6\u5939"},{"level":2,"title":"\u7B2C\u4E94\u6B65\uFF0C\u542F\u52A8mongodb\u670D\u52A1","slug":"\u7B2C\u4E94\u6B65\uFF0C\u542F\u52A8mongodb\u670D\u52A1"},{"level":2,"title":"\u7B2C\u516D\u6B65\uFF0C\u901A\u8FC7windows\u670D\u52A1\u6765\u7BA1\u7406MongoDB\u7684\u542F\u52A8\u548C\u5173\u95ED","slug":"\u7B2C\u516D\u6B65\uFF0C\u901A\u8FC7windows\u670D\u52A1\u6765\u7BA1\u7406mongodb\u7684\u542F\u52A8\u548C\u5173\u95ED"},{"level":2,"title":"\u8BED\u6CD5","slug":"\u8BED\u6CD5"}],"relativePath":"notes/6.other/5.just-start/2.mongodb.md","lastUpdated":1646925997929}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 18);
+const _hoisted_19 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_19);
+}
+var _2_mongodb = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _2_mongodb as default };
