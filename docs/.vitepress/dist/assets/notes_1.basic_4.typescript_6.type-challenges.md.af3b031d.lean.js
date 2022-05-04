840fbfd52af2df4e75112c92ecf279a86a785c27
zqylzcwcxy <347891134@qq.com>
2022-05-04 18:12:58 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_1.basic_4.typescript_6.type-challenges.md.af3b031d.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.92049e59.js";
+const __pageData = '{"title":"type-challenges","description":"","frontmatter":{},"headers":[{"level":2,"title":"warm-up","slug":"warm-up"},{"level":2,"title":"13.helloworld","slug":"_13-helloworld"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54"},{"level":2,"title":"easy","slug":"easy"},{"level":2,"title":"4.pick","slug":"_4-pick"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-1"},{"level":2,"title":"7.Readonly","slug":"_7-readonly"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-2"},{"level":2,"title":"11.Tuple to Object","slug":"_11-tuple-to-object"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-3"},{"level":2,"title":"14.First of Array","slug":"_14-first-of-array"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-4"},{"level":2,"title":"18.Length of Tuple","slug":"_18-length-of-tuple"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-5"},{"level":2,"title":"43.Exclude","slug":"_43-exclude"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-6"},{"level":2,"title":"189.Awaited","slug":"_189-awaited"},{"level":3,"title":"\u89E3\u7B54","slug":"\u89E3\u7B54-7"},{"level":2,"title":"","slug":""},{"level":2,"title":"268.if","slug":"_268-if"},{"level":2,"title":"Extract","slug":"extract"},{"level":3,"title":"\u7528\u6CD5","slug":"\u7528\u6CD5"},{"level":3,"title":"\u5B9E\u73B0\u65B9\u5F0F","slug":"\u5B9E\u73B0\u65B9\u5F0F"}],"relativePath":"notes/1.basic/4.typescript/6.type-challenges.md","lastUpdated":1651645715310}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 70);
+const _hoisted_71 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_71);
+}
+var _6_typeChallenges = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _6_typeChallenges as default };
