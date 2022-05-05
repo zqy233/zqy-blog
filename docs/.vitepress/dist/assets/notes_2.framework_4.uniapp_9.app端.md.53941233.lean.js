17f4225ed300530e16b5f0b9fe30b1c5a6ec0d3d
zqylzcwcxy <347891134@qq.com>
2022-05-05 18:33:23 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_2.framework_4.uniapp_9.app端.md.53941233.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.cf8f4a5b.js";
+const __pageData = '{"title":"app\u7AEF\u4E0B\u8F7D\u56FE\u7247","description":"","frontmatter":{},"headers":[{"level":2,"title":"canvas\u4FDD\u5B58\u56FE\u7247","slug":"canvas\u4FDD\u5B58\u56FE\u7247"},{"level":2,"title":"base64\u8F6C\u6362\u56FE\u7247","slug":"base64\u8F6C\u6362\u56FE\u7247"},{"level":2,"title":"\u4E0B\u8F7D\u56FE\u7247\u81F3\u624B\u673A\u76F8\u518C","slug":"\u4E0B\u8F7D\u56FE\u7247\u81F3\u624B\u673A\u76F8\u518C"},{"level":2,"title":"\u521B\u5EFA\u6216\u6253\u5F00\u6587\u4EF6","slug":"\u521B\u5EFA\u6216\u6253\u5F00\u6587\u4EF6"},{"level":2,"title":"\u83B7\u53D6\u624B\u673A\u7CFB\u7EDF\u6240\u6709\u76EE\u5F55\u6587\u4EF6","slug":"\u83B7\u53D6\u624B\u673A\u7CFB\u7EDF\u6240\u6709\u76EE\u5F55\u6587\u4EF6"},{"level":2,"title":"\u5220\u9664\u6587\u4EF6","slug":"\u5220\u9664\u6587\u4EF6"}],"relativePath":"notes/2.framework/4.uniapp/9.app\u7AEF.md","lastUpdated":1650715507991}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 16);
+const _hoisted_17 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_17);
+}
+var _9_app_ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _9_app_ as default };
