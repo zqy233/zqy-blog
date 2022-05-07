cad923efe2644f18b9134499f46b97ff2f6a654a
zqylzcwcxy <347891134@qq.com>
2022-05-07 23:09:51 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_2.framework_1.vue_6.pinna.md.968c5e89.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.95793c00.js";
+const __pageData = '{"title":"pinia","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u4F7F\u7528","slug":"\u4F7F\u7528"},{"level":2,"title":"\u521B\u5EFAstore\u72B6\u6001\u7BA1\u7406\u5E93","slug":"\u521B\u5EFAstore\u72B6\u6001\u7BA1\u7406\u5E93"},{"level":2,"title":"\u8BFB\u53D6\u6570\u636E","slug":"\u8BFB\u53D6\u6570\u636E"},{"level":2,"title":"\u4FEE\u6539\u6570\u636E","slug":"\u4FEE\u6539\u6570\u636E"},{"level":2,"title":"\u4FEE\u6539\u6570\u636E\u7684\u53E6\u5916\u4E09\u79CD\u65B9\u5F0F","slug":"\u4FEE\u6539\u6570\u636E\u7684\u53E6\u5916\u4E09\u79CD\u65B9\u5F0F"},{"level":2,"title":"getters","slug":"getters"},{"level":2,"title":"\u591A\u4E2Astore","slug":"\u591A\u4E2Astore"},{"level":2,"title":"vue-devtools\u8C03\u8BD5pinna","slug":"vue-devtools\u8C03\u8BD5pinna"}],"relativePath":"notes/2.framework/1.vue/6.pinna.md","lastUpdated":1650526195899}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 54);
+const _hoisted_55 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_55);
+}
+var _6_pinna = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _6_pinna as default };
