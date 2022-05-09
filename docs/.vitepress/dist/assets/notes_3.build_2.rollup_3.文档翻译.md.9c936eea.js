1a98a9e22bd4ea797c8c264bf5a0d4e27c4dd913
zqylzcwcxy <347891134@qq.com>
2022-05-09 22:23:10 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_3.build_2.rollup_3.文档翻译.md.9c936eea.js
@@ -0,0 +1,8 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock } from "./app.91e2525d.js";
+const __pageData = '{"title":"","description":"","frontmatter":{},"relativePath":"notes/2.framework/1.vue/5.vite\u63D2\u4EF6\u5F00\u53D1.md","lastUpdated":1650522669432}';
+const _sfc_main = {};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div");
+}
+var _3_____ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _3_____ as default };
