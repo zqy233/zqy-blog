1df25d018b7ee25015950eefe70164b85a6354d0
zqylzcwcxy <347891134@qq.com>
2022-05-10 23:23:53 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_6.other_11.页面设计_3.测试.md.908d6396.js
@@ -0,0 +1,26 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, b as createBaseVNode, e as createTextVNode } from "./app.9b038787.js";
+const __pageData = '{"title":"\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177","slug":"\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177"}],"relativePath":"notes/6.other/11.\u9875\u9762\u8BBE\u8BA1/3.\u6D4B\u8BD5.md","lastUpdated":1652178168686}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h2", {
+  id: "\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177 "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#\u79FB\u52A8\u7AEF\u6D4B\u8BD5\u5DE5\u5177",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, "vconsole", -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("p", null, "eruda", -1);
+const _hoisted_4 = [
+  _hoisted_1,
+  _hoisted_2,
+  _hoisted_3
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_4);
+}
+var _3___ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _3___ as default };
