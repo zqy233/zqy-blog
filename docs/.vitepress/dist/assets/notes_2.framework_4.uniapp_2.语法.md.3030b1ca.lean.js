8def994b53e3b50a08c466fc24cfeefb26278109
zqylzcwcxy <347891134@qq.com>
2022-05-13 22:46:56 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_2.framework_4.uniapp_2.语法.md.3030b1ca.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.234e94c4.js";
+const __pageData = '{"title":"\u8BED\u6CD5","description":"","frontmatter":{},"headers":[{"level":2,"title":"pages.json","slug":"pages-json"},{"level":3,"title":"pages\u8BBE\u7F6E\u9875\u9762","slug":"pages\u8BBE\u7F6E\u9875\u9762"},{"level":3,"title":"\u53BB\u9664\u9ED8\u8BA4\u6807\u9898","slug":"\u53BB\u9664\u9ED8\u8BA4\u6807\u9898"},{"level":3,"title":"globalStyle","slug":"globalstyle"},{"level":3,"title":"tabBar","slug":"tabbar"},{"level":2,"title":"uniapp\u4F7F\u7528scss","slug":"uniapp\u4F7F\u7528scss"},{"level":2,"title":"upx/rpx","slug":"upx-rpx"},{"level":2,"title":"tap/click","slug":"tap-click"},{"level":2,"title":"easycom\u7EC4\u4EF6","slug":"easycom\u7EC4\u4EF6"},{"level":2,"title":"\u6761\u4EF6\u7F16\u8BD1","slug":"\u6761\u4EF6\u7F16\u8BD1"},{"level":2,"title":"webview","slug":"webview"}],"relativePath":"notes/2.framework/4.uniapp/2.\u8BED\u6CD5.md","lastUpdated":1652006306213}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 43);
+const _hoisted_44 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_44);
+}
+var _2___ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _2___ as default };
---
+++ docs/.vitepress/dist/assets/notes_3.build_3.babel_3.babel.md.f3d5a71f.lean.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.234e94c4.js";
+const __pageData = '{"title":"babel","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5165\u95E8\u4F7F\u7528","slug":"\u5165\u95E8\u4F7F\u7528"},{"level":2,"title":"babel.config.js","slug":"babel-config-js"},{"level":2,"title":"polyfill","slug":"polyfill"}],"relativePath":"notes/3.build/3.babel/3.babel.md","lastUpdated":1646448802485}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 21);
+const _hoisted_22 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_22);
+}
+var _3_babel = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _3_babel as default };
