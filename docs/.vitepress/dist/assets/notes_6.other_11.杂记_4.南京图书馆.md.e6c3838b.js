b8e7a321a769d8a83910c62e0924f79463ed98c7
zqylzcwcxy <347891134@qq.com>
2022-05-17 16:43:40 +0800

    ':memo:撰写文档'

---
+++ docs/.vitepress/dist/assets/notes_6.other_11.杂记_4.南京图书馆.md.e6c3838b.js
@@ -0,0 +1,38 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, b as createBaseVNode, e as createTextVNode } from "./app.ae952a35.js";
+const __pageData = '{"title":"\u5357\u4EAC\u56FE\u4E66\u9986","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u5357\u4EAC\u56FE\u4E66\u9986","slug":"\u5357\u4EAC\u56FE\u4E66\u9986"}],"relativePath":"notes/6.other/11.\u6742\u8BB0/4.\u5357\u4EAC\u56FE\u4E66\u9986.md","lastUpdated":1652408960689}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("h2", {
+  id: "\u5357\u4EAC\u56FE\u4E66\u9986",
+  tabindex: "-1"
+}, [
+  /* @__PURE__ */ createTextVNode("\u5357\u4EAC\u56FE\u4E66\u9986 "),
+  /* @__PURE__ */ createBaseVNode("a", {
+    class: "header-anchor",
+    href: "#\u5357\u4EAC\u56FE\u4E66\u9986",
+    "aria-hidden": "true"
+  }, "#")
+], -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("p", null, "\u5730\u5740\u5357\u4EAC\u5E02\u4E2D\u5C71\u4E1C\u8DEF189\u53F7", -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("p", null, [
+  /* @__PURE__ */ createBaseVNode("a", {
+    href: "http://www.jslib.org.cn/fw/fwzn/cjwt/",
+    target: "_blank",
+    rel: "noopener noreferrer"
+  }, "http://www.jslib.org.cn/fw/fwzn/cjwt/")
+], -1);
+const _hoisted_4 = /* @__PURE__ */ createBaseVNode("div", { class: "language-" }, [
+  /* @__PURE__ */ createBaseVNode("pre", null, [
+    /* @__PURE__ */ createBaseVNode("code", null, " npm uninstall @vue/cli-plugin-eslint babel-eslint eslint eslint-plugin-vue\n")
+  ])
+], -1);
+const _hoisted_5 = [
+  _hoisted_1,
+  _hoisted_2,
+  _hoisted_3,
+  _hoisted_4
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_5);
+}
+var _4______ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, _4______ as default };
---
+++ docs/.vitepress/dist/assets/notes_6.other_4.solve-problems_postmon.md.410bef7e.js
@@ -0,0 +1,12 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a as createStaticVNode } from "./app.ae952a35.js";
+const __pageData = '{"title":"postmon","description":"","frontmatter":{},"headers":[{"level":2,"title":"tunneling socket could not be established, cause=connect ECONNREFUSED 192.168.18.17:8080","slug":"tunneling-socket-could-not-be-established-cause-connect-econnrefused-192-168-18-17-8080"}],"relativePath":"notes/6.other/4.solve-problems/postmon.md","lastUpdated":1648542958486}';
+const _sfc_main = {};
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode('<h1 id="postmon" tabindex="-1">postmon <a class="header-anchor" href="#postmon" aria-hidden="true">#</a></h1><h2 id="tunneling-socket-could-not-be-established-cause-connect-econnrefused-192-168-18-17-8080" tabindex="-1">tunneling <a href="https://gitee.com/link?target=https%3A%2F%2Fso.csdn.net%2Fso%2Fsearch%3Fq%3Dsocket%26spm%3D1001.2101.3001.7020" target="_blank" rel="noopener noreferrer">socket</a> could not be established, cause=connect ECONNREFUSED 192.168.18.17:8080 <a class="header-anchor" href="#tunneling-socket-could-not-be-established-cause-connect-econnrefused-192-168-18-17-8080" aria-hidden="true">#</a></h2><p>\u8BBF\u95EE\u80FD\u6B63\u5E38\u8BBF\u95EE\u7684\u63A5\u53E3\uFF0C\u4E00\u76F4\u62A5\u9519</p><p>postman\u5DE6\u4E0A\u89D2file-&gt;settings-&gt;proxy-&gt;\u53BB\u6389\u6240\u6709proxy\u9009\u9879</p>', 4);
+const _hoisted_5 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_5);
+}
+var postmon = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export { __pageData, postmon as default };
