0a1e65368b52d83cee82103284272f2a6589f94f
zqylzcwcxy <347891134@qq.com>
2022-06-08 20:31:51 +0800

    :memo:document writing

--- blog/docs/assets/notes_5.library_prettier_2.npm方式.md.79a53971.lean.js
+++
@@ -1,8 +0,0 @@
-import { _ as _export_sfc, c as createElementBlock, o as openBlock } from "./app.8f5e902d.js";
-const __pageData = '{"title":"","description":"","frontmatter":{},"relativePath":"notes/2.framework/1.vue/5.vite\u63D2\u4EF6\u5F00\u53D1.md","lastUpdated":1652939653952}';
-const _sfc_main = {};
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return openBlock(), createElementBlock("div");
-}
-var _2_npm__ = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
-export { __pageData, _2_npm__ as default };
