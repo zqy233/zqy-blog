3deea69fcf0b73fc4b8d16b8f4e5370a86aa9020
zqylzcwcxy <347891134@qq.com>
2022-06-10 16:38:40 +0800

    :memo:document writing

--- index.js
+++
@@ -1,3 +0,0 @@
-const { exec } = require('child_process')
-// 输出当前目录（不一定是代码所在的目录）下的文件和文件夹
-exec('yarn dev')
--- src/script/index.js
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-const shell = require('shelljs') // 脚本执行
-shell.exec('git add .')
-shell.exec("git commit -m ':memo:撰写文档'")
-shell.exec('git push')
