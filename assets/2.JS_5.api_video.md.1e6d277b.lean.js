1539c1c99e50ccae49d05adf5449e7044f389d28
zqy233 <zqy233@users.noreply.github.com>
2023-09-15 06:32:24 +0000

    deploy: 4a88518f31a877742b63c932f43ce57e497ba65d

---
+++ assets/2.JS_5.api_video.md.1e6d277b.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as l}from"./app.8bfa11d3.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"2.JS/5.api/video.md","lastUpdated":1694759414000}'),p={name:"2.JS/5.api/video.md"},o=l("",1),e=[o];function c(t,r,B,y,i,F){return a(),n("div",null,e)}const u=s(p,[["render",c]]);export{m as __pageData,u as default};
